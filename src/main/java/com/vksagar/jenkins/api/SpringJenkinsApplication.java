e75e429b68f1899fd0a01ba0853d782339313d1e
unknown <Vikram Kshirsagar@DESKTOP-24PCHH7>
2021-07-14 08:51:49 +0530

    Init commit

---
+++ src/main/java/com/vksagar/jenkins/api/SpringJenkinsApplication.java
@@ -0,0 +1,25 @@
+package com.vksagar.jenkins.api;
+
+import javax.annotation.PostConstruct;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJenkinsApplication {
+	
+	static Logger logger = LoggerFactory.getLogger(SpringJenkinsApplication.class);
+	
+	@PostConstruct
+	public void init() {
+		logger.info("++++ In init() ++++");
+	}
+
+	public static void main(String[] args) {
+		logger.info("++++ In main() ++++");
+		SpringApplication.run(SpringJenkinsApplication.class, args);
+	}
+
+}
